3dbfd7fecf4c54907cab94250a7d8ed567bd9bca
avivacaller <aviva@percayso.com>
2017-08-02 10:36:55 +0100

    adding some mui elements

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import RaisedButton from 'material-ui/RaisedButton'
+
+import NavDrawer from '../components/Navdrawer'
 
 injectTapEventPlugin()
 
@@ -10,13 +11,7 @@ class Template extends Component{
         return(
             <MuiThemeProvider>
             <div>
-                <header>
-                    <h1>TicTacTuring</h1>
-                    <RaisedButton
-                        label="Test Button"
-                        primary={true}
-                    onTouchTap={()=>{console.log("hello, i work")}}/>
-                </header>
+                <NavDrawer/>
                 <main>
                     {this.props.children}
                 </main>
